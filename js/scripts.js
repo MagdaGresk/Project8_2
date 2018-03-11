5a9215ba23abeafb604d951e7b6b504c11f67dc7
Magdalena Gresk <magdagresk@onet.pl>
2018-03-11 11:01:03 -0400

    Add comments in js file

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,6 @@
 // scripts.js
 
 var name = prompt('Enter your name');
-alert('Hello, ' + name);
-console.log('Hello, ' + name);
+alert('Hello, ' + name); //this is requested comment
+/*this is another requested comment*/
+console.log('Hello, ' + name); //this is comment number 3
