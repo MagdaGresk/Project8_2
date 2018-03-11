d57c0b0108eaf1142557f576237db71345f31b10
Magdalena Gresk <magdagresk@onet.pl>
2018-03-11 11:10:45 -0400

    Add and modify comments in js file

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
 // scripts.js
 
 var name = prompt('Enter your name');
-alert('Hello, ' + name); //this is requested comment
-/*this is another requested comment*/
+alert('Hello, ' + name); //this is requested comment number 1
+/*this is another requested comment number 2*/
 console.log('Hello, ' + name); //this is comment number 3
